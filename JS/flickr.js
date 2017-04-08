9eda0db477e2b28bea9aa56792ed89f1f6b9b923
sanchez1179 <icsanchez1179@gmail.com>
2017-04-08 03:46:38 -0700

    actually working

--- JS/flickr.js
+++ JS/flickr.js
@@ -1,23 +1,31 @@
-$(document).ready(function(){
-  $('button').click(function () {
+$(document).ready(function() {
+
+
+ $('button').click(function () {
+    // highlight the button
+    // not AJAX, just cool looking
     $("button").removeClass("selected");
-   $(this).addClass("selected");
-    var flickerAPI="http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    $(this).addClass("selected");
+
+    // the AJAX part
+    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     var animal = $(this).text();
     var flickrOptions = {
       tags: animal,
       format: "json"
     };
-    function displayPhotos(data){
+    function displayPhotos(data) {
       var photoHTML = '<ul>';
-      $.each(data.items, function(i, photo){
+      $.each(data.items,function(i,photo) {
         photoHTML += '<li class="grid-25 tablet-grid-50">';
         photoHTML += '<a href="' + photo.link + '" class="image">';
-        photoHTML += '<img src="' + photo.media.m + '" >'</a><li>
-      });
-       photoHTML += '</ul>';
-       $('#photos').html(photoHTML);
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      }); // end each
+      photoHTML += '</ul>';
+      $('#photos').html(photoHTML);
     }
-    $.getJSON(flickerAPI,flickrOptions,displayPhotos);
-  });
-});
+    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+
+  }); // end click
+
+}); // end ready
